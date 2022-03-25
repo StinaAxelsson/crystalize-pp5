27b248539dfb2967017604c755cf441e2d86c987
StinaAxelsson <stina91@live.se>
2022-03-25 10:49:09 +0000

    added messages for views and templates for error, success, info and warning toasts

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
 from products.models import Product
 
 
@@ -9,14 +10,18 @@ def view_cart(request):
 
 def add_to_cart(request, item_id):
     """ Add a product and quantity of it in the shopping cart"""
+
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
+        messages.success(request, f'Successfully added {product.name} to your shoppingcart!')
 
     request.session['cart'] = cart
     
@@ -32,8 +37,10 @@ def change_cart(request, item_id):
 
     if quantity > 0:
         cart[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {cart[item_id]}')
     else:
         cart.pop[item_id]
+        messages.success(request, f'Deleted {product.name} from your cart')
 
     request.session['cart'] = cart
     
@@ -47,9 +54,12 @@ def delete_in_cart(request, item_id):
     try:
         cart = request.session.get('cart', {})
         cart.pop(item_id)
+        messages.success(request, f'Deleted {product.name} from your cart')
+
         request.session['cart'] = cart
 
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error! Remove item {e} from cart ')
         return HttpResponse(status=500)
