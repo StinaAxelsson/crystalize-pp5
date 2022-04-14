d76d304cfed6c7d7d8ff75d1077ddf839a79141b
StinaAxelsson <stina91@live.se>
2022-04-14 11:39:40 +0000

    fix homepage styling

--- wishlist/models.py
+++ wishlist/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from products.models import Product
+from profiles.models import UserProfile
 
 
 class WishList(models.Model):
@@ -7,3 +8,7 @@ class WishList(models.Model):
     Model for saved items in a wishlist
     """
     product = models.ForeignKey(Product, on_delete=models.CASCADE, default=1)
+    logged_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return f'Wishlist ({self.logged_user})'
--- wishlist/views.py
+++ wishlist/views.py
@@ -1,28 +1,33 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from products.models import Product
 from wishlist.models import WishList
+from profiles.models import UserProfile
 
 
+@login_required
 def wishlist(request):
     """Show wishlist"""
-
-    template = 'wishlist/wishlist.html'
-    wishlist = WishList.objects.all()
+    user = get_object_or_404(UserProfile, user=request.user)
+    wishlist = WishList.objects.filter(logged_user=user)
 
     context = {
         'wishlist': wishlist,
     }
+    template = 'wishlist/wishlist.html'
     return render(request, template, context)
 
 
+@login_required
 def save_to_wishlist(request, product_id):
     """ Save a product to a wishlist """
-
+    user = get_object_or_404(UserProfile, user=request.user)
     product = get_object_or_404(Product, pk=product_id)
 
     WishList.objects.create(
-        product=product
+        product=product,
+        logged_user=user,
     )
 
     messages.success(request, f'{product.name} has successfully saved to your wishlist')
@@ -30,9 +35,11 @@ def save_to_wishlist(request, product_id):
     return redirect(reverse('product_detail', args=[product.id]))
 
 
+@login_required
 def delete_in_wishlist(request, product_id):
     """ Delete a product from wishlist """
 
+    user = get_object_or_404(UserProfile, user=request.user)
     product = get_object_or_404(Product, pk=product_id)
     WishList.objects.filter(product=product).delete()
 
